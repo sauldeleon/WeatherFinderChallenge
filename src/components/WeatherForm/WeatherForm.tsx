c1a5e115e4044c7662adb1ad17ed8dd641af0efa
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2021-01-16 14:04:57 +0100

    feat: add service layer, use useState on WeatherForm

--- src/components/WeatherForm/WeatherForm.tsx
+++ src/components/WeatherForm/WeatherForm.tsx
@@ -1,34 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 
+import { fetchCurrentWeather } from '../../service/open-weather'
 import { WeatherContext, WeatherContextStoreType } from '../../store/WeatherContext'
 import { BaseButton } from '../BaseButton'
 import { BaseInput } from '../BaseInput'
 
-const { REACT_APP_OPENWEATHERMAP_API_KEY } = process.env
-
 export const WeatherForm: React.FC = () => {
+  const [city, setCity] = useState<string>('')
+  const [country, setCountry] = useState<string>('')
   const { setWeatherInfo, setError } = useContext(WeatherContext) as WeatherContextStoreType
 
   const getWeather = async (e: React.FormEvent) => {
     e.preventDefault()
-    const target = e.target as typeof e.target & {
-      city: { value: string }
-      country: { value: string }
-    }
-    const city = target.city.value
-    const country = target.country.value
-    const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${REACT_APP_OPENWEATHERMAP_API_KEY}&units=metric`,
-    )
-    const data = await api_call.json()
     if (city && country) {
-      setWeatherInfo({
-        temperature: data.main.temp,
-        location: `${data.name},  ${data.sys.country}`,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-      })
-      setError(undefined)
+      const { data, error } = await fetchCurrentWeather(city, country)
+      if (error) {
+        setError(error)
+      } else {
+        setWeatherInfo({
+          temperature: data.main.temp,
+          location: `${data.name},  ${data.sys.country}`,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+        })
+        setError(undefined)
+      }
     } else {
       setWeatherInfo({
         temperature: undefined,
@@ -41,8 +37,16 @@ export const WeatherForm: React.FC = () => {
   }
   return (
     <form onSubmit={getWeather}>
-      <BaseInput name={'city'} placeholder="Madrid" />
-      <BaseInput name={'country'} placeholder="es" />
+      <BaseInput
+        name={'city'}
+        placeholder="Madrid"
+        onChange={(e: React.FormEvent<HTMLInputElement>) => setCity(e.currentTarget.value)}
+      />
+      <BaseInput
+        name={'country'}
+        placeholder="es"
+        onChange={(e: React.FormEvent<HTMLInputElement>) => setCountry(e.currentTarget.value)}
+      />
       <BaseButton label="Get Weather" />
     </form>
   )
